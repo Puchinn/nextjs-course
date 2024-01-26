04c92eb4c09afd138fc1eb9e2b72b92b7950a871
Esteban Sayago <sayagoo823@gmail.com>
2024-01-26 19:09:16 -0300

    update page

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -1,7 +1,6 @@
-import Link from "next/link";
-
 import api from "@/api";
 
+import RestaurantCard from "./components/RestaurantCard/index";
 import SearchBox from "./components/SearchBox";
 
 export default async function HomePage({searchParams}: {searchParams: {q: string}}) {
@@ -12,24 +11,7 @@ export default async function HomePage({searchParams}: {searchParams: {q: string
       <SearchBox />
       {restaurants.length === 0 && "not founded"}
       {restaurants.map((restaurant) => {
-        return (
-          <Link key={restaurant.id} href={`/${restaurant.id}`} prefetch={false}>
-            <img
-              alt={restaurant.name}
-              className="mb-3 h-[300px] w-full object-cover"
-              src={restaurant.image}
-            />
-            <h2 className="inline-flex gap-2 text-lg font-bold">
-              <span>{restaurant.name}</span>
-              <small className="inline-flex gap-1">
-                <span>⭐</span>
-                <span>{restaurant.score}</span>
-                <span className="font-normal opacity-75">({restaurant.ratings})</span>
-              </small>
-            </h2>
-            <p className="opacity-90">{restaurant.description}</p>
-          </Link>
-        );
+        return <RestaurantCard key={restaurant.id} restaurant={restaurant} />;
       })}
     </section>
   );
